baa8da8d8cec1de7d524421607ee5471cc6626b0
Robert (PC) <rwallhead@gmail.com>
2015-09-16 20:51:47 +1000

    Rewrote code to just use telnet and /dev/stdout as a "buffer" for the image

--- src/get_frame.py
+++ src/get_frame.py
@@ -1,76 +1,48 @@
 """ Parrot Jumping Sumo frame grabber.
 """
+import base64
 import socket
 import telnetlib
-import threading
-import SocketServer
 
 
 PARROT_IP = '192.168.2.1'
-LISTEN_PORT = 49999
 
 
-def snapshot(ip_addr, listen_port, width=160, height=120, timeout=1):
+def snapshot(ip_addr, width=160, height=120, timeout=1):
     """ Returns raw JPEG data from Parrot jumping sumo.
     """
-
-    # Determine my IP address on the interface connected to the sumo
-    try:
-        my_ip = [ip
-                 for ip
-                 in socket.gethostbyname_ex(socket.gethostname())[2]
-                 if ip.startswith('.'.join(ip_addr.split('.')[:-1]) + '.')][0]
-    except IndexError:
-        raise Exception(
-            'Cannot determine IP address assigned by Parrot Jumping Sumo.'
-        )
-
-
-    # Build a handler that can shutdown after one request
-    img_data = []
-    server = None
-    class Handler(SocketServer.BaseRequestHandler):
-        """ Handler for connections from the server.
-        """
-        def handle(self):
-            try:
-                img_data.append(self.request.recv(102400)) # 100KB
-            finally:
-                threading.Thread(target=server.shutdown).start()
-
-    # Start a server in a thread with that handler
-    server = SocketServer.TCPServer(('', listen_port), Handler)
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-
     # Connect to the sumo and request the image
     try:
         tconn = telnetlib.Telnet(ip_addr, timeout=timeout)
-        tconn.read_until('[JS] $', 1)
-        tconn.write('kill `pidof dragon-prog`2>/dev/null\n')
+        tconn.read_until('[JS] $ ')
+        tconn.write('kill `pidof dragon-prog` 2> /dev/null\n')
+        tconn.read_until('[JS] $ ')
         tconn.write(
             ' '.join((
+                'echo \'\' > /dev/stdout',
+                ';',
                 'yavta',
-                '--skip=1',
+                '--skip=2',
                 '-s{}x{}'.format(width, height),
-                '-c2',
-                '-S{}:{}'.format(my_ip, listen_port),
+                '-c1',
+                '-F/dev/stdout',
                 '-fMJPEG',
                 '/dev/video0',
-            )) + '>/dev/null\n'
+                '> /dev/null',
+                ';',
+                'base64 /dev/stdout',
+            )) + '\r\n'
         )
     except socket.timeout:
         raise Exception('Failed to connect to Jumping Sumo and request image.')
 
-    # Wait on image being received
-    server_thread.join(timeout)
-    if server_thread.isAlive():
-        raise Exception('No image received from Jumping Sumo.')
+    res = tconn.read_until(
+        '[JS] $ '
+    ).split('base64 /dev/stdout')[1].replace('\r\n', '')[:-7]
 
-    return img_data[0]
+    return base64.b64decode(res)
 
 
 if __name__ == '__main__':
     with open('sumo.jpg', 'wb') as f:
-        f.write(snapshot(PARROT_IP, LISTEN_PORT))
+        f.write(snapshot(PARROT_IP))
