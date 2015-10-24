0c2d82b9c21d5eab27bf57c1629395da136da181
Robert Wallhead <rwallhead@gmail.com>
2015-10-24 15:40:30 +1100

    Significantly better video preview using sniffing of controller->bot video.
    Will look at injecting video trigger ack/movement controls later, but this is a much better place for the project to be than yesterday (the xmlrpcsumo + websockets implementation was toooooo slow for the RPi by a long shot).

--- sumocharge/run.py
+++ sumocharge/run.py
@@ -1,17 +1,21 @@
 """ Robot docking management system.
 """
 import multiprocessing
+import operator
+import time
 
 
-def p_sumo_interface():
-    """ Fire up the xmlrpc interface to the sumo.
+def p_sumo_proxy():
+    """ Fire up the proxy - this will spit UDP packets at 127.0.0.1:65432.
     """
-    import sumopy.xmlrpcsumo
-    sumopy.xmlrpcsumo.start()
+    import sumoproxy.proxy
+    sumoproxy.proxy.proc_wrapper()
 
 
 def p_www():
     """ Fire up the web viewer/controller.
+
+        Sits on 0.0.0.0:8000
     """
     import www
     www.launch()
@@ -20,9 +24,27 @@ def p_www():
 def main():
     """ Run all the things.
     """
-    multiprocessing.Process(target=p_sumo_interface).start()
+    targets = (
+        p_sumo_proxy,
+        p_www,
+    )
 
-    multiprocessing.Process(target=p_www).start()
+    # Create the procs
+    procs = [multiprocessing.Process(target=target)
+             for target
+             in targets]
+
+    # Start them
+    map(operator.methodcaller('start'), procs)
+
+    # If any are dead, start them
+    while True:
+        for i, proc in enumerate(procs):
+            if not proc.is_alive():
+                proc.terminate()
+                procs[i] = multiprocessing.Process(target=targets[i])
+                procs[i].start()
+            time.sleep(0.1)
 
 
 if __name__ == '__main__':
--- sumocharge/www.py
+++ sumocharge/www.py
@@ -1,39 +1,24 @@
 """ Flask-based control console.
-
-    Testing:
-        ../venv/bin/gunicorn -w 4 --worker-class socketio.sgunicorn.GeventSocketIOWorker -b 0.0.0.0:5000 www:app
-
 """
 # Generic imports
 import base64
+import collections
 import flask
-import xmlrpclib
+import threading
+import SocketServer
 
 # Create the flask app
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'  # TODO: change...
 
+
 # Create the socketio wrapper
 import flask.ext.socketio
 socketio = flask.ext.socketio.SocketIO(app)
 
-# Create the sumo client
-sumoclient = xmlrpclib.ServerProxy('http://127.0.0.1:8000')
 
-
-## Websocket endpoints
-@socketio.on('request_frame', namespace='/video')
-def request_frame():
-    frame = None
-    try:
-        frame = sumoclient.pic()
-    except Exception:
-        return
-    if frame is None:
-        return
-    flask.ext.socketio.emit('frame', {
-        'data': base64.b64encode(frame.data),
-    })
+# Thread/process-safe queue of images
+image_queue = collections.deque(maxlen=30)
 
 
 ## URL endpoints
@@ -44,8 +29,43 @@ def index():
     return flask.render_template('index.html')
 
 
+## Websocket endpoints
+@socketio.on('request_frame', namespace='/video')
+def request_frame():
+    try:
+        flask.ext.socketio.emit('frame', {
+            'data': image_queue.pop(),
+        })
+    except IndexError:
+        pass
+
+
+## General methods
+def start_video_thread(sockio_mod):
+
+    class Handler(SocketServer.BaseRequestHandler):
+
+        def handle(self):
+            data = self.request[0]
+
+            # ID pic frames
+            if data.startswith('<\x03\x7d'):
+                image_queue.append(base64.b64encode(data[13:]))
+
+    # Patch to recieve video packets (+ direction indicator)
+    SocketServer.UDPServer.max_packet_size = 65001
+    server = SocketServer.UDPServer(('127.0.0.1', 65432), Handler)
+    t = threading.Thread(target=server.serve_forever)
+    t.daemon = True
+    t.start()
+
+
 def launch():
-    socketio.run(app)
+    # Start the thread that will emit video frames as they arrive
+    start_video_thread(flask.ext.socketio)
+
+    # Start the web app
+    socketio.run(app, host='0.0.0.0', port=8000)
 
 
 if __name__ == '__main__':
